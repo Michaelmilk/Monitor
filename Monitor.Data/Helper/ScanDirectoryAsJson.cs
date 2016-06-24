6bdc7457a8795332c28b1e9de3f54567bbfe9c70
Jixiong Ge <jixge@microsoft.com>
2016-06-24 21:12:07 +0800

    1606242110

--- Monitor.Data/Helper/ScanDirectoryAsJson.cs
+++ Monitor.Data/Helper/ScanDirectoryAsJson.cs
@@ -53,3 +53,62 @@ namespace Monitor.Data.Helper
         }
     }
 }
+
+public class ScanDirectoryAsJson2
+{
+    public string label { get; set; }
+    public string picturePath { get; set; }
+    //public string pictureName { get; set; }
+    //public bool isFolder { get; set; }
+    //public string key { get; set; }
+    public string configPath { get; set; }
+    public List<ScanDirectoryAsJson2> children { get; set; }//child directory tree
+
+    //public ScanDirectoryAsJson2(ScanDirectoryAsJson2 parent, FileSystemInfo fileSystemInfo)
+    public ScanDirectoryAsJson2(FileSystemInfo fileSystemInfo)
+    {
+        label = fileSystemInfo.Name;
+        children = new List<ScanDirectoryAsJson2>();
+
+        if (fileSystemInfo.Attributes == FileAttributes.Directory)
+        {
+            //isFolder = true;
+            foreach (FileSystemInfo f in (fileSystemInfo as DirectoryInfo).GetFileSystemInfos())
+            {
+                if (f.Attributes == FileAttributes.Directory)
+                {
+                    children.Add(new ScanDirectoryAsJson2(f));
+                }
+
+                else
+                {
+                    //isFolder = false;
+                    if (Array.IndexOf(new string[] { ".jpg", ".bmp", ".png" }, f.Extension) != -1)
+                    {
+                        picturePath = f.FullName;
+                    }
+
+                    if (f.Extension == ".prj")
+                    {
+                        configPath = f.FullName;
+                    }
+                }
+            }
+        }
+        //else
+        //{
+        //    //isFolder = false;
+        //    if (Array.IndexOf(new string[] { ".jpg", ".bmp", ".png" }, fileSystemInfo.Extension) != -1)
+        //    {
+        //        parent.picturePath = fileSystemInfo.FullName;
+        //    }
+        //}
+
+        //key = label.Replace(" ", "").ToLower();
+    }
+
+    public new string ToString()
+    {
+        return JsonConvert.SerializeObject(this, Formatting.Indented);
+    }
+}
